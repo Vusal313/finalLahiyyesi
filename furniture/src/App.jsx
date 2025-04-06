89230a107053bc8f3a53be283d5ac8bc30a0d016
Vusal313 <muradovvusal565@gmail.com>
2025-04-06 20:37:12 +0400

    home

--- furniture/src/App.jsx
+++ furniture/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Foother from "./components/Foother";
+import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import About from "./pages/About";
 import News from "./pages/News";
@@ -21,7 +22,7 @@ function App() {
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Shop />} />
+          <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/about" element={<About />} />
           <Route path="/news" element={<News />} />
--- furniture/src/pages/Home.jsx
+++ furniture/src/pages/Home.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
-
-export default function Home() {
+import React from "react";
+import "./Home.scss";
+export default  function Home  (){
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="home__left">
+        <h1>
+          Exquisite design <br /> combined with <br /> functionalities
+        </h1>
+        <p>
+          Pellentesque ullamcorper dignissim condimentum volutpat consequat
+          mauris nunc lacinia quis.
+        </p>
+
+        <div className="home__contact">
+          <div className="avatars">
+            <img src="../../public/img/Furniture_files/home-avatar-1.webp" alt="#" />
+            <img src="../../public/img/Furniture_files/home-avatar-2.webp" alt="#" />
+            <img src="../../public/img/Furniture_files/home-avatar-3.webp" alt="#" />
+          </div>
+          <span>Contact with our expert</span>
+        </div>
+
+        <button className="home__shop">Shop Now</button>
+      </div>
+
+      <div className="home__right">
+        <div className="product-card main">
+          <div className="label">
+            
+            <h4>Wooden Chair</h4>
+            <p>$199</p>
+          </div>
+          <button className="arrow">→</button>
+        </div>
+
+        <div className="side-cards">
+          <div className="product-card">
+            <div className="label">
+              <h4>Pretium Elite</h4>
+              <p>$130</p>
+            </div>
+            <button className="arrow">→</button>
+          </div>
+
+          <div className="discount-card">
+            <h3>25% OFF</h3>
+            <p>Donec ac odio tempor dapibus.</p>
+            <button>Explore Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
